0781a57d6a7d640e3766dc786e75346546de73ad
hiroshi.imaizumi <hiroshi.imaizumi@bigtreetc.com>
2024-12-21 11:21:24 +0900

    variable

---
+++ variable.py
@@ -0,0 +1,13 @@
+num = 1
+name = "Mike"
+is_ok = True
+
+print(num, type(num))
+print(name, type(name))
+print(is_ok, type(is_ok))
+
+string_to_int = int("1")
+print(string_to_int, type(is_ok))
+
+type_num: int = 10
+print(type_num, type(type_num))
