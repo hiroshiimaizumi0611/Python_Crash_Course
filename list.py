9033f539e6b6b01b512cf157097fc456571b9549
hiroshi.imaizumi <hiroshi.imaizumi@bigtreetc.com>
2024-12-22 23:55:34 +0900

    list

---
+++ list.py
@@ -0,0 +1,13 @@
+l = [1, 20, 4, 50, 2, 1, 2]
+print(l[0])
+print(l[-1])
+print(l[-2])
+print(l[0:2])
+print(l[:2])
+print(l[2:])
+print(l[:])
+print(len(l))
+
+n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(n[::2])
+print(n[::-1])
